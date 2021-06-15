4eb976cdff020caf7cc106f5fba080bd61732665
Giovanni Caiazzo <gionny93@gmail.com>
2021-06-15 15:03:51 +0200

    Added export of slider and prettified the files. Also added scripts and changed dependencies

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MultiRangeSlider from './js/MultiRangeSlider';
 
+Object.defineProperty(exports, 'MultiRangeSlider', {
+    enumerable: true,
+    get: function get() {
+        return MultiRangeSlider.default;
+    },
+});
+
 const App = () => {
     return <MultiRangeSlider init_ranges={[]} />;
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 const htmlPlugin = new HtmlWebPackPlugin({
-  template: "./src/index.html",
-  filename: "./index.html",
+    template: './src/index.html',
+    filename: './index.html',
 });
 module.exports = {
-  mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-  plugins: [htmlPlugin],
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [htmlPlugin],
 };
